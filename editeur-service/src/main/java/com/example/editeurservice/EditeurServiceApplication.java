9028d9edb8e9b91dda0f083b66a14248b201e91c
Lorhaki <74306283+Lorhaki@users.noreply.github.com>
2024-01-04 19:26:38 +0100

    Add files via upload

---
+++ editeur-service/src/main/java/com/example/editeurservice/EditeurServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.editeurservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditeurServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditeurServiceApplication.class, args);
+    }
+
+}
