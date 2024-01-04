a9abc70e762e334dab1fe3b51f6d2082858c5547
Lorhaki <74306283+Lorhaki@users.noreply.github.com>
2024-01-05 00:21:14 +0100

    Add files via upload

---
+++ jeux-service/src/main/java/com/example/jeuxservice/JeuxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jeuxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeuxServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeuxServiceApplication.class, args);
+    }
+
+}
